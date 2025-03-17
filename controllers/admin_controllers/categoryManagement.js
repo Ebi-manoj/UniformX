fc05f6fc71723a81f26a6d080b8786880b810029
Ebi manoj <ebimanoj28@gmail.com>
2025-03-17 20:26:21 +0530

    get Category

--- controllers/admin_controllers/categoryManagement.js
+++ controllers/admin_controllers/categoryManagement.js
@@ -2,10 +2,32 @@ import asyncHandler from 'express-async-handler';
 import { Category } from '../../model/category_model.js';
 
 export const getCategory = asyncHandler(async (req, res) => {
+  const page = parseInt(req.query.page) || 1;
+  const limit = 10;
+  const skip = (page - 1) * limit;
+  const searchQuery = req.query.search?.trim() || '';
+
+  let query = { isActive: true };
+  if (searchQuery) {
+    query.name = { $regex: searchQuery, $options: 'i' };
+  }
+  const totalCategories = await Category.countDocuments(query);
+  const totalPages = Math.ceil(totalCategories / limit);
+
+  // Fetch categories
+  const categories = await Category.find(query)
+    .sort({ createdAt: -1 })
+    .skip(skip)
+    .limit(limit);
   res.render('admin/categoryManage', {
     category: 'main',
     cssFile: 'user_manage',
     js_file: 'category',
+    categories,
+    currentPage: page,
+    totalPages,
+    totalCategories,
+    searchQuery,
   });
 });
 export const addCategory = asyncHandler(async (req, res) => {
